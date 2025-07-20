a5fdda6b9a96ba90083f2831dcd462e31cef2141
Mohamed <mohamed.ali.awad2018@gmail.com>
2025-07-21 00:26:44 +0300

    TO Q07

--- Assignment 05/Program.cs
+++ Assignment 05/Program.cs
@@ -21,31 +21,174 @@
 #endregion
 
 #region Q02
+//Console.WriteLine("Please enter the length of the arrays");
+//int length = int.TryParse(Console.ReadLine(), out int result) ? result : 0;
+//if (length <= 0)
+//{
+//    Console.WriteLine("Invalid length. Please enter a positive integer.");
+//    return;
+//}
+//int[] array1 = new int[length];
+//int[] array2 = new int[length];
+//int[] mergedArray = new int[length * 2];
+
+//for (int i = 0; i < length; i++)
+//{
+//    Console.WriteLine($"Please enter number {i + 1} for the first array");
+//    array1[i] = int.Parse(Console.ReadLine()!);
+//}
+
+//for (int i = 0; i < length; i++)
+//{
+//    Console.WriteLine($"Please enter number {i + 1} for the second array");
+//    array2[i] = int.Parse(Console.ReadLine()!);
+//}
+
+//mergedArray = array1.Concat(array2).ToArray();
+//Array.Sort(mergedArray);
+//Console.WriteLine("The merged and sorted array is:");
+//foreach (var item in mergedArray)
+//{
+//    Console.WriteLine(item);
+//}
 
 #endregion
 
 
 #region Q03
 
+
+
+
+
 #endregion
 
 
 
-#region Q04
+#region Q04 
+//Console.WriteLine("Please enter the length of the array");
+//int length = int.TryParse(Console.ReadLine(), out int result) ? result : 0;
+//if (length <= 0)
+//{
+//    Console.WriteLine("Invalid length. Please enter a positive integer.");
+//    return;
+//}
+//int[] numbers = new int[length];
+//for (int i = 0; i < length; i++)
+//{
+//    Console.WriteLine($"Please enter number {i + 1}");
+//    numbers[i] = int.Parse(Console.ReadLine()!);
+//}
+//int max = 0;
+//int min = int.MaxValue;
+
+//for (int i = 0; i < length; i++)
+//{
+
+//        if (numbers[i] > max)
+//        {
+//            max = numbers[i];
+//        }
+//        if (numbers[i] < min)
+//        {
+//            min = numbers[i];
+//        }
+//    }
+//Console.WriteLine($"Max is {max}, and Min is {min}");
+
 
 #endregion
 
 
 
 #region Q05
+//Console.WriteLine("Please enter the length of the array");
+//int length = int.TryParse(Console.ReadLine(), out int result) ? result : 0;
+//if (length <= 0)
+//{
+//    Console.WriteLine("Invalid length. Please enter a positive integer.");
+//    return;
+//}
+//int[] numbers = new int[length];
+//for (int i = 0; i < length; i++)
+//{
+//    Console.WriteLine($"Please enter number {i + 1}");
+//    numbers[i] = int.Parse(Console.ReadLine()!);
+//}
+
+//Array.Sort(numbers);
+//Array.Reverse(numbers);
+//if (numbers.Length < 2)
+//{
+//    Console.WriteLine("Not enough numbers to find the second largest.");
+//    return;
+//}
+//Console.WriteLine($"The 2nd Larget number is:{numbers[1]}");
 
 #endregion
 
 
 #region Q06
 
+// Console.WriteLine("Please enter the length of the arrays");
+//int length = int.TryParse(Console.ReadLine(), out int result) ? result : 0;
+//if (length <= 0)
+//{
+//    Console.WriteLine("Invalid length. Please enter a positive integer.");
+//    return;
+//}
+//int[] array = new int[length];
+
+
+//for (int i = 0; i < length; i++)
+//{
+//    Console.WriteLine($"Please enter number {i + 1} for the first array");
+//    array[i] = int.Parse(Console.ReadLine()!);
+//}
+
+//    Console.WriteLine("The array is:");
+//    foreach (var item in array)
+//    {
+//        Console.WriteLine(item);
+//    }
+
+
 #endregion
 
-#region Q07
+#region Q07 
+
+
+#endregion
+
+
+#region Q08
+
+
+
+
+#endregion
+
+
+
+#region Q09
+
+#endregion
+
+
+#region Q10
+
+#endregion
+
+
+#region Q11
+
+#endregion
+
+
+#region Q12
+
+#endregion
+
+#region Q13
 
 #endregion
