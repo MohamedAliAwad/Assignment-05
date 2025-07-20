3134489a60f817048c71d4a7afabf3f01b30befe
moham <moham@DESKTOP-PI8PSJF>
2025-07-20 22:18:33 +0300

    Add project files.

---
+++ Assignment 05/Program.cs
@@ -0,0 +1,51 @@
+﻿#region Q01
+//Console.WriteLine("Please enter the arry length");
+//int length = int.TryParse(Console.ReadLine(), out int result) ? result : 0;
+//int[] numbers = new int[length];
+//int sum = 0;
+//if (length <= 0)
+//{
+//    Console.WriteLine("Invalid length. Please enter a positive integer.");
+//    return;
+//}
+//else
+//{
+//    for (int i = 0; i < length; i++)
+//    {
+//        Console.WriteLine($"Please enter number {i + 1}");
+//        numbers[i] = int.Parse(Console.ReadLine()!);
+//        sum += numbers[i];
+//    }
+//    Console.WriteLine($"The sum of the numbers is: {sum}");
+//}
+#endregion
+
+#region Q02
+
+#endregion
+
+
+#region Q03
+
+#endregion
+
+
+
+#region Q04
+
+#endregion
+
+
+
+#region Q05
+
+#endregion
+
+
+#region Q06
+
+#endregion
+
+#region Q07
+
+#endregion
